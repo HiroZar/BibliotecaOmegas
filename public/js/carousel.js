76a8a898e0886d8dca1fdbc693c6968c6b511843
HiroZar <hirozarhz@gmail.com>
2023-12-03 08:46:45 -0500

    v1.2

--- public/js/carousel.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('./Bloques/carousel.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('myCarousel').innerHTML = data;
-      });
-  });
--- public/js/header.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('./Bloques/header.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('myHeader').innerHTML = data;
-      });
-  });
